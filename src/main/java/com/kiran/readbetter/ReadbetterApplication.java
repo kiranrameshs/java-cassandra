24edd88f7e8922a6a80af2df130f819d4f210cff
Kiran Ramesh S <kiranrs2@gmail.com>
2022-02-04 21:30:27 -0500

    base folder

---
+++ src/main/java/com/kiran/readbetter/ReadbetterApplication.java
@@ -0,0 +1,13 @@
+package com.kiran.readbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadbetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadbetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiran/readbetter/ReadbetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiran.readbetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadbetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
